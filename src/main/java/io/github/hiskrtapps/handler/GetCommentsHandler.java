ab1006b96c90a896bfb6f6524a9870028f9b38f4
Manuela ☺ <giampaolo.grieco@gmail.com>
2021-01-14 18:35:30 +0100

    first commit; handlers stubs creation

--- src/main/java/io/github/hiskrtapps/handler/GetCommentsHandler.java
+++ src/main/java/io/github/hiskrtapps/handler/GetCommentsHandler.java
@@ -1,8 +1,7 @@
-package com.aws.codestar.projecttemplates.handler;
+package io.github.hiskrtapps.handler;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
-
 import com.aws.codestar.projecttemplates.GatewayResponse;
 import org.json.JSONObject;
 
@@ -12,11 +11,11 @@ import java.util.Map;
 /**
  * Handler for requests to Lambda function.
  */
-public class HelloWorldHandler implements RequestHandler<Object, Object> {
+public class GetCommentsHandler implements RequestHandler<Object, Object> {
 
     public Object handleRequest(final Object input, final Context context) {
         Map<String, String> headers = new HashMap<>();
         headers.put("Content-Type", "application/json");
-        return new GatewayResponse(new JSONObject().put("Output", "Hello World!").toString(), headers, 200);
+        return new GatewayResponse(new JSONObject().put("Output", input.toString()).toString(), headers, 200);
     }
 }

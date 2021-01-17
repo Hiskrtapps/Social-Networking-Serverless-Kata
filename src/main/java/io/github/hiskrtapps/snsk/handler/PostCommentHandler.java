c9f14fab1d6b8638ca6d631ca6d18d9260b95efb
Manuela ☺ <giampaolo.grieco@gmail.com>
2021-01-17 12:16:05 +0100

    reading user from token + refactor

--- src/main/java/io/github/hiskrtapps/snsk/handler/PostCommentHandler.java
+++ src/main/java/io/github/hiskrtapps/snsk/handler/PostCommentHandler.java
@@ -1,12 +1,12 @@
 package io.github.hiskrtapps.snsk.handler;
 
-import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import io.github.hiskrtapps.snsk.infrastructure.GatewayResponse;
 import io.github.hiskrtapps.snsk.model.Message;
+import org.apache.commons.codec.binary.Base64;
 import org.json.JSONObject;
 
 import java.time.LocalDateTime;
@@ -15,7 +15,10 @@ import java.time.ZonedDateTime;
 import java.util.HashMap;
 import java.util.Map;
 
+import static com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder.standard;
+import static java.lang.Long.MAX_VALUE;
 import static java.time.LocalDateTime.now;
+import static java.time.ZoneId.systemDefault;
 import static java.time.format.DateTimeFormatter.ISO_DATE_TIME;
 
 /**
@@ -24,33 +27,35 @@ import static java.time.format.DateTimeFormatter.ISO_DATE_TIME;
 public class PostCommentHandler implements RequestHandler<Map<Object, Object>, Object> {
 
     public Object handleRequest(final Map<Object, Object> input, final Context context) {
-        context.getLogger().log(String.format("Input: %s", input));
-        context.getLogger().log(String.format("new JSONObject(input): %s", new JSONObject(input)));
-        context.getLogger().log(String.format("new JSONObject().put(\"I\", input): %s", new JSONObject().put("I", input)));
+        saveMessage(buildMessage(input));
+        return response();
+    }
 
-        JSONObject body = new JSONObject(new JSONObject(input).getString("body"));
-
-        AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard().build();
-
-        DynamoDBMapper mapper = new DynamoDBMapper(client);
-
-        LocalDateTime now = now();
-        long recentness = ZonedDateTime.of(now, ZoneId.systemDefault()).toInstant().toEpochMilli();
-
-        //JSONObject body = new JSONObject(input);
-
-        Message message = new Message();
-        message.setRecentness(Long.MAX_VALUE - recentness);
-        message.setMessage(body.getString("message"));
-        message.setCreatedAt(ISO_DATE_TIME.format(now()));
-        message.setUserId(body.getString("userId"));
-        message.setStatus("OK");
-
-        mapper.save(message);
-
-        Map<String, String> headers = new HashMap<>();
+    private Object response() {
+        final Map<String, String> headers = new HashMap<>();
         headers.put("Content-Type", "application/json");
-        return new GatewayResponse(new JSONObject().put("Output", input.toString()).toString(), headers, 200);
+        return new GatewayResponse("", headers, 200);
+    }
+
+    private void saveMessage(final Message message) {
+        new DynamoDBMapper(standard().build()).save(message);
+    }
+
+    private Message buildMessage(final Map<Object, Object> input) {
+        final JSONObject jInput = new JSONObject(input);
+        final LocalDateTime now = now();
+        long recentness = ZonedDateTime.of(now, systemDefault()).toInstant().toEpochMilli();
+        final Message message = new Message();
+        message.setRecentness(MAX_VALUE - recentness);
+        message.setMessage(new JSONObject(jInput.getString("body")).getString("message"));
+        message.setCreatedAt(ISO_DATE_TIME.format(now));
+        message.setUserId(userId(jInput.getJSONObject("headers").getString("authorization")));
+        message.setStatus("OK");
+        return message;
+    }
+
+    private String userId(final String token) {
+        return new JSONObject(new String(new Base64(true).decode(token.split("\\.")[1]))).getString("email");
     }
 
 }
